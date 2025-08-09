1d151df278f3783e4c08b8b24637d971fc136d34
Mireia <mireiagonzalezz03@gmail.com>
2025-08-09 21:53:30 +0200

    created level 4 with moving hazard

--- player.gd
+++ player.gd
@@ -4,6 +4,8 @@ extends RigidBody3D
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export var torque_thrust: float = 100.0
 
+var is_transitioning: bool = false
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
@@ -13,14 +15,27 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 func _on_body_entered(body: Node) -> void:
-	var groups = body.get_groups()
-	if "Goal" in groups:
-		complete_level(body.file_path)
-	if "Hazard" in groups:
-		crash_sequence()
+	if !is_transitioning:
+		var groups = body.get_groups()
+		if "Goal" in groups:
+			complete_level(body.file_path)
+		if "Hazard" in groups:
+			crash_sequence()
 
 func crash_sequence() -> void:
-	get_tree().reload_current_scene()
+	print("crashed")
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1)
+	tween.tween_callback(get_tree().reload_current_scene)
 
 func complete_level(next_level_file: String) -> void:
-	get_tree().change_scene_to_file(next_level_file)
+	print("Level complete")
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1)
+	tween.tween_callback(
+		get_tree().change_scene_to_file.bind(next_level_file)
+	)
