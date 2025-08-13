3ab5efb68a1d605a7fb771f571d05440e48023c3
Mireia <mireiagonzalezz03@gmail.com>
2025-08-13 15:21:08 +0200

    let player quit game by pressing esc

--- player.gd
+++ player.gd
@@ -36,6 +36,9 @@ func _process(delta: float) -> void:
 		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 	else:
 		left_booster_particles.emitting = false
+	
+	if Input.is_action_pressed("ui_cancel"):
+		get_tree().quit()
 
 func _on_body_entered(body: Node) -> void:
 	if !is_transitioning:
